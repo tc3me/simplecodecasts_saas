cbee733c51d088e5591755631030ca8d9c4da3d7
Tom Carroll <tc3@tc3.me>
2015-07-18 18:04:53 +0000

    Fixed typo in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devis::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
     
     def create
         super do |resource|
